7b8334964eeeece1d865ac108ff50132e07b55cd
siid sharma <siid123ck@gmail.com>
2024-05-23 17:10:30 +1000

    frontend files are strucutred

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/governance/Governor.sol";
@@ -8,131 +9,100 @@ import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
 
-contract GovernorContract is
-	Governor,
-	GovernorSettings,
-	GovernorCountingSimple,
-	GovernorVotes,
-	GovernorVotesQuorumFraction,
-	GovernorTimelockControl
-{
-	constructor(
-		IVotes _token,
-		TimelockController _timelock,
-		uint256 _votingDelay,
-		uint256 _votingPeriod,
-		uint256 _quorumPercentage
-	)
-		Governor("GovernorContract")
-		GovernorSettings(
-			_votingDelay, /* 1 block */
-			_votingPeriod, // 45818, /* 1 week */
-			0 // proposal threshold
-		)
-		GovernorVotes(_token)
-		GovernorVotesQuorumFraction(_quorumPercentage)
-		GovernorTimelockControl(_timelock)
-	{}
+contract MyGovernor is Governor, GovernorSettings, GovernorCountingSimple, GovernorVotes, GovernorVotesQuorumFraction, GovernorTimelockControl {
+    constructor(IVotes _token, TimelockController _timelock)
+        Governor("MyGovernor")
+        GovernorSettings(7200 /* 1 day */, 50400 /* 1 week */, 0)
+        GovernorVotes(_token)
+        GovernorVotesQuorumFraction(4)
+        GovernorTimelockControl(_timelock)
+    {}
 
-	// The following functions are overrides required by Solidity.
+    // The following functions are overrides required by Solidity.
 
-	function votingDelay()
-		public
-		view
-		override(IGovernor, GovernorSettings)
-		returns (uint256)
-	{
-		return super.votingDelay();
-	}
+    function votingDelay()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingDelay();
+    }
 
-	function votingPeriod()
-		public
-		view
-		override(IGovernor, GovernorSettings)
-		returns (uint256)
-	{
-		return super.votingPeriod();
-	}
+    function votingPeriod()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.votingPeriod();
+    }
 
-	function quorum(uint256 blockNumber)
-		public
-		view
-		override(IGovernor, GovernorVotesQuorumFraction)
-		returns (uint256)
-	{
-		return super.quorum(blockNumber);
-	}
+    function quorum(uint256 blockNumber)
+        public
+        view
+        override(Governor, GovernorVotesQuorumFraction)
+        returns (uint256)
+    {
+        return super.quorum(blockNumber);
+    }
 
-	function getVotes(address account, uint256 blockNumber)
-		public
-		view
-		override(IGovernor, Governor)
-		returns (uint256)
-	{
-		return super.getVotes(account, blockNumber);
-	}
+    function state(uint256 proposalId)
+        public
+        view
+        override(Governor, GovernorTimelockControl)
+        returns (ProposalState)
+    {
+        return super.state(proposalId);
+    }
 
-	function state(uint256 proposalId)
-		public
-		view
-		override(Governor, GovernorTimelockControl)
-		returns (ProposalState)
-	{
-		return super.state(proposalId);
-	}
+    function proposalNeedsQueuing(uint256 proposalId)
+        public
+        view
+        override(Governor, GovernorTimelockControl)
+        returns (bool)
+    {
+        return super.proposalNeedsQueuing(proposalId);
+    }
 
-	function propose(
-		address[] memory targets,
-		uint256[] memory values,
-		bytes[] memory calldatas,
-		string memory description
-	) public override(Governor, IGovernor) returns (uint256) {
-		return super.propose(targets, values, calldatas, description);
-	}
+    function proposalThreshold()
+        public
+        view
+        override(Governor, GovernorSettings)
+        returns (uint256)
+    {
+        return super.proposalThreshold();
+    }
 
-	function proposalThreshold()
-		public
-		view
-		override(Governor, GovernorSettings)
-		returns (uint256)
-	{
-		return super.proposalThreshold();
-	}
+    function _queueOperations(uint256 proposalId, address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
+        internal
+        override(Governor, GovernorTimelockControl)
+        returns (uint48)
+    {
+        return super._queueOperations(proposalId, targets, values, calldatas, descriptionHash);
+    }
 
-	function _execute(
-		uint256 proposalId,
-		address[] memory targets,
-		uint256[] memory values,
-		bytes[] memory calldatas,
-		bytes32 descriptionHash
-	) internal override(Governor, GovernorTimelockControl) {
-		super._execute(proposalId, targets, values, calldatas, descriptionHash);
-	}
+    function _executeOperations(uint256 proposalId, address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
+        internal
+        override(Governor, GovernorTimelockControl)
+    {
+        super._executeOperations(proposalId, targets, values, calldatas, descriptionHash);
+    }
 
-	function _cancel(
-		address[] memory targets,
-		uint256[] memory values,
-		bytes[] memory calldatas,
-		bytes32 descriptionHash
-	) internal override(Governor, GovernorTimelockControl) returns (uint256) {
-		return super._cancel(targets, values, calldatas, descriptionHash);
-	}
+    function _cancel(address[] memory targets, uint256[] memory values, bytes[] memory calldatas, bytes32 descriptionHash)
+        internal
+        override(Governor, GovernorTimelockControl)
+        returns (uint256)
+    {
+        return super._cancel(targets, values, calldatas, descriptionHash);
+    }
 
-	function _executor()
-		internal
-		view
-		override(Governor, GovernorTimelockControl)
-		returns (address)
-	{
-		return super._executor();
-	}
-
-	function supportsInterface(bytes4 interfaceId)
-		public
-		view
-		override(Governor, GovernorTimelockControl)
-		returns (bool)
-	{
-		return super.supportsInterface(interfaceId);
-	}
+    function _executor()
+        internal
+        view
+        override(Governor, GovernorTimelockControl)
+        returns (address)
+    {
+        return super._executor();
+    }
 }
--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -1,42 +1,52 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
-// import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-// import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-// contract GovernanceToken is ERC20, ERC20Permit {
+contract GovernanceToken is ERC20, ERC20Permit, ERC20Votes {
 
-//     uint256 public s_maxSupply = 1000000000000000000000000;
+    uint256 public s_maxSupply = 1000000000000000000000000;
 
-//     constructor(
-//         string memory _name,
-//         string memory _symbol
-//     )
-//         ERC20(_name, _symbol)
-//         ERC20Permit(_name)
-//     {
-//         _mint(msg.sender, s_maxSupply);
-//     }
+    constructor(
+        string memory _name,
+        string memory _symbol
+    )
+        ERC20(_name, _symbol)
+        ERC20Permit(_name)
+    {
+        _mint(msg.sender, s_maxSupply);
+    }
 
-//     // The functions below are overrides required by Solidity.
 
-//     function _afterTokenTransfer(
-//         address from,
-//         address to,
-//         uint256 amount
-//     ) internal (ERC20, ERC20Votes) {
-//         super._afterTokenTransfer(from, to, amount);
-//     }
 
-//     function _mint(address to, uint256 amount) internal
-//     override(ERC20, ERC20Votes) {
-//         super._mint(to, amount);
-//     }
+    // The functions below are overrides required by Solidity.
+       function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
 
-//     function _burn(address account, uint256 amount)
-//         internal
-//         override(ERC20, ERC20Votes)
-//     {
-//         super._burn(account, amount);
-//     }
-// }
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+
+    function mint(address to, uint256 amount)
+     {
+        super._mint(to, amount);
+    }
+
+    function burn(address account, uint256 amount)
+        internal virtual
+        override(ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+}
